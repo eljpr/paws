a3de5a6c1af7a0ff05ab01cbfabd3b72f1273397
Ella Pierre <ellajpierre@gmail.com>
2024-05-14 14:58:19 +0100

    added log validations

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,21 @@
 class Log < ApplicationRecord
   belongs_to :dog
+  validates :medication, inclusion: { in: %w[`none antibiotic antiparasitic antifungal steroids pain relief`],
+                                      message: `%<value> is not a valid form of medication` }
+  validates :food, inclusion: { in: %w[`kibble canned raw home-made`],
+                                message: `%<value> is not a valid form of food` }
+  validates :grooming, inclusion: { in: %w[`bath brush nail-trim ear-care fur-cut brush-teeth`],
+                                    message: `%<value> is not a valid form of grooming` }
+  validates :special_treat, inclusion: { in: %w[`crunchy soft jerky dental-chews animal-based rawhide home-made`],
+                                         message: `%<value> is not a valid form of treat` }
+  validates :stool, inclusion: { in: %w[`normal constipation diarrhea`],
+                                 message: `%<value> is not a valid stool input` }
+  validates :vaccination, inclusion: { in: %w[`none distemper parvovirus adenovirus leptospira)`],
+                                       message: `%<value> is not a valid vaccination` }
+  validates :mood, inclusion: { in: %w[`happy fearful anxious jealous aggressive`],
+                                message: `%<value> is not a valid mood` }
+  validates :energy, inclusion: { in: %w[`energertic ok tired exhausted calm excited`],
+                                  message: `%<value> is not a valid form of energy` }
+  validates :training, inclusion: { in: %w[`obedience therapy behavioural agility protection service tracking`],
+                                    message: `%<value> is not a valid training` }
 end
