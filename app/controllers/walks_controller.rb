a059007cdaf13526597c78ff11d316cf814b2bee
max cody <Max.cody1@outlook.com>
2024-05-14 14:16:37 +0100

    done a view page for the walk for the map to be located there

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -1,12 +1,12 @@
 class WalksController < ApplicationController
   def index
     @walks = Walk.all
-    @markers = @walks.geocoded.map do |walk|
-      {
-        lat: walk.latitude,
-        lng: walk.longitude
-      }
-    end
+    # @markers = @walks.geocoded.map do |walk|
+    #   {
+    #     lat: walk.latitude,
+    #     lng: walk.longitude
+    #   }
+    # end
     if params[:query].present?
       sql_subquery = <<~SQL
         bikes.location @@ :query
@@ -34,23 +34,4 @@ end
 def walk_params
   params.require(:walk).permit(:start_time, :end_time, :start_lat, :start_lng, :end_lat, :end_lng, :distance, :pace, :distance)
 end
-
-
-
-
-#reference of the table dont mind this
-
-# create_table "walks", force: :cascade do |t|
-#   t.time "start_time"
-#   t.time "end_time"
-#   t.float "pace"
-#   t.decimal "start_lat"
-#   t.decimal "start_lng"
-#   t.decimal "end_lat"
-#   t.decimal "end_lng"
-#   t.float "distance"
-#   t.bigint "dog_id", null: false
-#   t.datetime "created_at", null: false
-#   t.datetime "updated_at", null: false
-#   t.index ["dog_id"], name: "index_walks_on_dog_id"
-# end
+end
