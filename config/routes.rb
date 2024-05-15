c0f45f5fdeab41fbb9cd599e61e40319ff3a5b7a
Ella Pierre <ellajpierre@gmail.com>
2024-05-15 12:38:40 +0100

    made user show and index actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   devise_for :users
   root to: "pages#home"
+
+  resources :users, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
