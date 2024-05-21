4dddf1b7077f3de9e178be7d431b48ced1314b0d
Ella Pierre <ellajpierre@gmail.com>
2024-05-21 15:14:12 +0100

    added prescription tab to dog show view

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -24,6 +24,7 @@ class DogsController < ApplicationController
   def show
     @pending = @dog.vet_dogs.where(status: "pending")
     @accepted = @dog.vet_dogs.where(status: "accepted")
+    @prescriptions = @dog.prescriptions
   end
 
   def edit; end
