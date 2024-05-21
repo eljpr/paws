00b63611bb89924f4d4e452af320bdab3f0df601
Gilmar Margoti <gilmar.mmj@outlook.com>
2024-05-21 22:25:30 +0100

    Fixing issues for log page. Save, update and layout.

--- app/models/log.rb
+++ app/models/log.rb
@@ -10,13 +10,4 @@ class Log < ApplicationRecord
   TRAINING      = ["Obedience", "Therapy", "Behavioural", "Agility", "Protection", "Service", "Tracking"]
 
   belongs_to :dog
-  validates :medication, inclusion: { in: %w[None Antibiotic Antiparasitic Antifungal Steroids Pain Relief] }
-  validates :food, inclusion: { in: %w[Kibble Canned Raw Home-made] }
-  validates :grooming, inclusion: { in: %w[Bath Brush Nail-trim Ear-care Fur-cut Brush-teeth] }
-  validates :special_treat, inclusion: { in: %w[Crunchy Soft Jerky Dental-chews Animal-based Rawhide Home-made] }
-  validates :stool, inclusion: { in: %w[Normal Constipation Diarrhea] }
-  validates :vaccination, inclusion: { in: %w[None Distemper Parvovirus Adenovirus Leptospira] }
-  validates :mood, inclusion: { in: %w[Happy Fearful Anxious Jealous Aggressive] }
-  validates :energy, inclusion: { in: %w[Energertic Ok Tired Exhausted Calm Excited] }
-  validates :training, inclusion: { in: %w[Obedience Therapy Behavioural Agility Protection Service Tracking] }
 end
