55ccf9275cc4ce604ab6e8701597082fddd90ec7
Ella Pierre <ellajpierre@gmail.com>
2024-05-21 10:53:44 +0100

    added accepted patients

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,15 @@ class UsersController < ApplicationController
     @users = User.all
     @vets = User.where(is_vet: true)
     @owners = User.where(is_vet: false)
-    @patients = current_user.patients
+    @patients = current_user.patients.joins(:vet_dogs).where(vet_dogs: { status: "accepted" })
     # we get dogs from our association through vet_dogs
   end
 
   def show
     @user = User.find(params[:id])
     @vet_dog = VetDog.new
-    @pending = @user.vet_dogs.where(status: "pending")
-    @accepted = @user.vet_dogs.where(status: "accepted")
+    @no_vet = current_user.dogs.left_joins(:vet_dogs).where(vet_dogs: { id: nil })
+    @pending = current_user.vet_dogs.where(status: "pending")
+    @accepted = current_user.vet_dogs.where(status: "accepted")
   end
 end
